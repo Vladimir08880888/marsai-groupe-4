1656aefb3d6f2ff2b47e070f9d21f61b0ed6aaba
PolinaDubova <pharabadot@gmail.com>
2026-01-30 10:02:10 +0100

    creation page contact

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -14,6 +14,7 @@ import { Register } from "./pages/auth/Register.jsx";
 import { RoleGuard } from "./middlewares/RoleGuard.jsx";
 import GalerieDesFilmsPage from "./pages/Gallerie.jsx";
 import Videos from "./pages/admin/Videos.jsx";
+import Contact from "./pages/public/Contact.jsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,6 +33,7 @@ createRoot(document.getElementById("root")).render(
           <Route path="/" element={<PublicLayout />}>
             <Route index element={<Home />} />
             <Route path="/gallerie" element={<GalerieDesFilmsPage />} />
+            <Route path="/contact" element={<Contact />} />
             <Route path="/auth/login" element={<Login />} />
             <Route path="/auth/register" element={<Register />} />
           </Route>
---
+++ front/src/pages/public/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Contact() {
+
+    return (
+    
+    <>
+        <h1 className="bg-black text-white">POLINA</h1>
+    </>
+    
+    );
+
+}
