dc390732f33313767a2d8f76ebfb605e3fac6b8a
João (School) <joao.silva-coutinho@laplateforme.io>
2026-02-10 15:27:06 +0100

    feat: add event retrieval and deletion functions to EventController

--- back/src/controllers/EventController.js
+++ back/src/controllers/EventController.js
@@ -1,2 +1,33 @@
 import Event from "../models/Event.js"
 
+function getEvents(req, res) {
+    Event.findAll().then((events) => {
+        res.json(events);
+    });
+}
+
+
+function deleteEvent(req, res) {
+    const { id } = req.params;
+    Event.destroy({ where: { id } }). then (() => {
+        res.status(204).json({ message: "Evennement supprimé"})
+    });
+}
+
+function getEventById(req, res) {
+    const { id } = req.params;
+    Event.findOne({ where: { id } }).then((event) => {
+        if(event) {
+           res.json(event);
+        } else {
+      res.status(404).json({ error: "Evennement non trouvé" });
+    }
+    })
+}
+
+
+export default {
+    getEvents,
+    deleteEvent,
+    getEventById
+}
