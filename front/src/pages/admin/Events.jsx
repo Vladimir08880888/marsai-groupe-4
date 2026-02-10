4cba64721a341f28bac4c80f29a90df25579113b
João (School) <joao.silva-coutinho@laplateforme.io>
2026-02-10 15:11:25 +0100

    feat: implement event management functionality with API integration and UI updates

--- front/src/pages/admin/Events.jsx
+++ front/src/pages/admin/Events.jsx
@@ -1,5 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-
+import { useEffect, useState } from "react";
+import { deleteUser, getUsers, updateUser, getRoles, createUser } from "../../api/users.js";
+import { useMutation } from "@tanstack/react-query";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 function Events(){
     return <div>Events</div>
